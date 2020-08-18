6be8970457d7f1d1fefc7d42ff1ab5cbc1de156d
Rakshit Khandelwal <rakshitkhandelwal1234@gmail.com>
2020-08-18 20:48:14 +0530

    Updates

---
+++ precache-manifest.7f9f1cb9a39190e6fefd9e6a5f111ce9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda4e1601659f07687c2e0841027e224",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e527881a8d66e99dcea8",
+    "url": "/portfolio/static/css/main.36859be0.chunk.css"
+  },
+  {
+    "revision": "846c6b3b306914d7ff97",
+    "url": "/portfolio/static/js/2.864f26b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.864f26b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e527881a8d66e99dcea8",
+    "url": "/portfolio/static/js/main.24b40248.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "dc78b70518f26d9abf68a4bdb15c13a0",
+    "url": "/portfolio/static/media/profilepic.dc78b705.PNG"
+  }
+]);
